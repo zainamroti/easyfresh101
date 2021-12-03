28a2cc50dbd70429da727050b270a69e107d4365
zainamroti <sayedzainjeelani@gmail.com>
2021-12-03 13:56:59 +0500

    Add chakra datepicker.

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,12 @@
 import { Box, Button, chakra, FormControl, FormLabel, GridItem, Heading, Input, Select, SimpleGrid, Stack, Text, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
+import { SingleDatepicker } from 'chakra-dayzed-datepicker';
+import React, { useState } from 'react'
 import Table from './Table';
 
 function Form() {
+    const [date, setDate] = useState(new Date());
+
+
     return (
         <Box mt={[10, 0]}>
             <chakra.form
@@ -30,8 +34,8 @@ function Form() {
                             </FormLabel>
                             <Input
                                 type="text"
-                                name="first_name"
-                                id="first_name"
+                                name="Customer_name"
+                                id="Customer_name"
                                 autoComplete="given-name"
                                 mt={1}
                                 focusBorderColor="brand.400"
@@ -44,7 +48,7 @@ function Form() {
 
                         <FormControl as={GridItem} colSpan={[6, 3]}>
                             <FormLabel
-                                htmlFor="Customer_address"
+                                htmlFor="order_taker"
                                 fontSize="sm"
                                 fontWeight="md"
                                 color={"gray.700"}
@@ -53,9 +57,9 @@ function Form() {
                             </FormLabel>
                             <Input
                                 type="text"
-                                name="last_name"
-                                id="last_name"
-                                autoComplete="family-name"
+                                name="order_taker"
+                                id="order_taker"
+                                autoComplete="taker-name"
                                 mt={1}
                                 focusBorderColor="brand.400"
                                 shadow="sm"
@@ -67,7 +71,7 @@ function Form() {
 
                         <FormControl as={GridItem} colSpan={6}>
                             <FormLabel
-                                htmlFor="street_address"
+                                htmlFor="customer_address"
                                 fontSize="sm"
                                 fontWeight="md"
                                 color={"gray.700"}
@@ -76,9 +80,9 @@ function Form() {
                             </FormLabel>
                             <Input
                                 type="text"
-                                name="street_address"
-                                id="street_address"
-                                autoComplete="street-address"
+                                name="customer_address"
+                                id="customer_address"
+                                autoComplete="customer_address"
                                 mt={1}
                                 focusBorderColor="brand.400"
                                 shadow="sm"
@@ -90,24 +94,17 @@ function Form() {
 
                         <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
                             <FormLabel
-                                htmlFor="city"
+                                htmlFor="delivery-date"
                                 fontSize="sm"
                                 fontWeight="md"
                                 color={"gray.700"}
                             >
                                 Delivery Date
                             </FormLabel>
-                            <Input
-                                type="text"
-                                name="city"
-                                id="city"
-                                autoComplete="city"
-                                mt={1}
-                                focusBorderColor="brand.400"
-                                shadow="sm"
-                                size="sm"
-                                w="full"
-                                rounded="md"
+                            <SingleDatepicker
+                                name="date-input"
+                                date={date}
+                                onDateChange={setDate}
                             />
                         </FormControl>
 
@@ -125,7 +122,7 @@ function Form() {
                     <Button
                         type="submit"
                         color="blue.500"
-                    bg={"gray.50"}
+                        bg={"gray.50"}
 
                         _focus={{ shadow: "" }}
                         variant='outline'
